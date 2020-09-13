f9ec195a80c39cf0091170fa3c47d45dc356f4d0
clbnlsn <mr.oscar.rugs@gmail.com>
2020-09-13 15:45:24 -0600

    initial commit

---
+++ src/components/Four.jsx
@@ -0,0 +1,11 @@
+import React,{useState} from 'react';
+
+const Four = (props) => {
+    return(
+        <div>
+            {props.four}
+        </div>
+    )
+}
+
+export default Four;
---
+++ src/components/Hello.jsx
@@ -0,0 +1,11 @@
+import React,{useState} from 'react';
+
+const Hello = (props) => {
+    return(
+        <div>
+            {props.hello}
+        </div>
+    )
+}
+
+export default Hello;
